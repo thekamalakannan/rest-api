6d86d7b0f3f60e7cce5f6e991251fd35e27ddf2c
Kamalakannan <kamalkuppus@gmail.com>
2024-01-20 15:45:30 +0000

    added JSON parsing from rest api call

--- src/main/java/olsol/bean/Todo.java
+++ src/main/java/olsol/bean/Todo.java
@@ -1,2 +1,10 @@
-package olsol.bean;public class Todo {
+package olsol.bean;
+
+public class Todo {
+    public int userId;
+    public int id;
+    public String title;
+    public boolean completed;
+    public String body;
+
 }
--- src/test/java/RestClientTest.java
+++ src/test/java/RestClientTest.java
@@ -1,16 +1,20 @@
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import olsol.bean.Todo;
 import olsol.flow.RestClient;
 import org.junit.Test;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
 public class RestClientTest {
 
+    public static String url = "https://jsonplaceholder.typicode.com/posts/1";
+    RestClient restClient = new RestClient();
+
     @Test
     public void testGetRequest() throws Exception {
-        RestClient restClient = new RestClient();
-        String url = "https://jsonplaceholder.typicode.com/posts/1";
         String expectedResponse = "{\n" +
                 "  \"userId\": 1,\n" +
                 "  \"id\": 1,\n" +
@@ -19,6 +23,22 @@ public class RestClientTest {
                 "}";
         String actualResponse = restClient.getRequest(url);
         assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    public void convertStringToJSONObject() throws IOException {
+        String jsonString = restClient.getRequest(url);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Todo todo;
+        try {
+            todo = objectMapper.readValue(jsonString, Todo.class);
+            assertEquals(1, todo.userId);
+            assertEquals(1, todo.id);
+            assertEquals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit", todo.title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
