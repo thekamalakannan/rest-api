8be49775b65b273ec88cf1d0ce54b4009a320ea9
Kamalakannan <kamalkuppus@gmail.com>
2024-06-15 15:04:34 +0100

    Log4j Added

--- src/main/java/olsol/flow/RestClient.java
+++ src/main/java/olsol/flow/RestClient.java
@@ -1,5 +1,8 @@
 package olsol.flow;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -13,12 +16,20 @@ public class RestClient {
 
     public String getRequest(String url) throws IOException {
 
+        Log log = LogFactory.getLog(RestClient.class.getName());
+        String responseEntity = null;
+
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet request = new HttpGet(url);
             HttpResponse response = httpClient.execute(request);
             HttpEntity entity = response.getEntity();
-            return entity != null ? EntityUtils.toString(entity) : null;
+            responseEntity =  entity != null ? EntityUtils.toString(entity) : null;
+            log.info("Response entity: "+responseEntity);
+        } catch (Exception e) {
+            log.error("Unable to establish connection", e);
         }
+
+        return responseEntity;
 
     }
 
